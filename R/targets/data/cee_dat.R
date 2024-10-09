591f180b81bf5f1f4e524405d91c67cfc889facd
Rob Schick <rss10@duke.edu>
2024-10-09 09:43:52 -0400

    Delete Hanks Cee Manifest
    Subset to just have 2019 cees

--- R/targets/data/cee_dat.R
+++ R/targets/data/cee_dat.R
@@ -12,7 +12,8 @@ register_target(
       cee_dat <- readr::read_csv(cee_file_location, show_col_types = FALSE) %>% 
         dplyr::mutate(deployid = stringr::str_remove(deployid, "_DUML")) %>% 
         dplyr::filter(deployid %in% exposed_ans,
-                      cee_type %in% c("simulated mfas", "control")) %>% 
+                      cee_type %in% c("simulated mfas", "control"),
+                      cee_id %in% c("19_01", "19_02", "19_03", "19_04")) %>% 
         dplyr::mutate(start_datetime = lubridate::ymd_hms(paste0(date_YYYYMMDD, start_HHMMSS))) %>% 
         dplyr::mutate(end_datetime = lubridate::ymd_hms(paste0(date_YYYYMMDD, end_HHMMSS)))
       
